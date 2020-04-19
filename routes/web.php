efd04010c179309a1179049917946b9bb65617b8
ordained07 <ordained07@gmail.com>
2020-04-19 18:58:05 +0100

    merging process of gabriel two

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,9 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/about/who-we-are', 'HomeController@whoweare')->name('whoweare');
-<<<<<<< HEAD
+
 Route::get('/virtual', 'HomeController@virtual')->name('virtual');
 Route::get('/facility/development', 'HomeController@facilityDevelopment')->name('development');
 Route::get('/parents-portal', 'HomeController@parentsPortal')->name('portal');
-=======
+
 Route::get('/about/board-of-directors', 'HomeController@boardofdirectors')->name('boardofdirectors');
->>>>>>> Gabriel
